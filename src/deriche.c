d964d22fac383d3c4eea93d899eb83a853ead63c
Marc-Antoine FERNANDES <embraser01@gmail.com>
2017-05-17 15:18:27 +0200

    Update version

--- src/deriche.c
+++ src/deriche.c
@@ -24,6 +24,15 @@ void *L5(void *data);
 
 void *L4(void *data);
 
+void *L1_line(void *data);
+
+void *L2_line(void *data);
+
+void *L4_line(void *data);
+
+void *L5_line(void *data)
+;
+
 void deriche_float(int width, int height)
 {
     int i, j;
@@ -87,12 +96,39 @@ void deriche_float(int width, int height)
 
 void *L1(void *data)
 {
-    int i, j;
+    int i;
 
     int *dataBis = data;
 
     int width = dataBis[0];
     int height = dataBis[1];
+
+    pthread_t *pCols = malloc(sizeof(pthread_t) * width);
+
+    // L1:
+
+    for (i = 0; i < width; i++)
+    {
+        int tab[2];
+        tab[0] = i;
+        tab[1] = height;
+        pthread_create(&pCols[i], NULL, L1_line, tab);
+    }
+
+    for (i = 0; i < width; i++)
+    {
+        pthread_join(pCols[i], NULL);
+    }
+    return NULL;
+}
+
+void *L1_line(void *data)
+{
+    int *dataBis = data;
+
+    int i = dataBis[0];
+    int height = dataBis[1];
+    int j;
 
     float xm1, ym1, ym2;
     float a1, a2;
@@ -103,30 +139,53 @@ void *L1(void *data)
     b1 = ((0.840896));
     b2 = (float) ((-0.606531));
 
-    // L1:
+    ym1 = 0, ym2 = 0, xm1 = 0;
 
-    for (i = 0; i < width; i++)
+    for (j = 0; j < height; j++)
     {
-        ym1 = 0, ym2 = 0, xm1 = 0;
-        for (j = 0; j < height; j++)
-        {
-            tmp1[i][j] = (a1 * in[i][j] + a2 * xm1 + b1 * ym1 + b2 * ym2);
-            xm1 = in[i][j];
-            ym2 = ym1;
-            ym1 = tmp1[i][j];
-        }
+        tmp1[i][j] = (a1 * in[i][j] + a2 * xm1 + b1 * ym1 + b2 * ym2);
+        xm1 = in[i][j];
+        ym2 = ym1;
+        ym1 = tmp1[i][j];
     }
     return NULL;
 }
 
 void *L2(void *data)
 {
-    int i, j;
+    int i;
 
     int *dataBis = data;
 
     int width = dataBis[0];
     int height = dataBis[1];
+
+    // L2:
+
+    pthread_t *pCols = malloc(sizeof(pthread_t) * width);
+
+    for (i = 0; i < width; i++)
+    {
+        int tab[2];
+        tab[0] = i;
+        tab[1] = height;
+        pthread_create(&pCols[i], NULL, L2_line, tab);
+    }
+
+    for (i = 0; i < width; i++)
+    {
+        pthread_join(pCols[i], NULL);
+    }
+    return NULL;
+}
+
+void *L2_line(void *data)
+{
+    int *dataBis = data;
+
+    int i = dataBis[0];
+    int height = dataBis[1];
+    int j;
 
     float xp1, xp2;
     float yp1, yp2;
@@ -139,19 +198,14 @@ void *L2(void *data)
     b2 = (float) ((-0.606531));
 
 
-    // L2:
-
-    for (i = 0; i < width; i++)
+    yp1 = 0, yp2 = 0, xp1 = 0, xp2 = 0;
+    for (j = height - 1; j >= 0; j--)
     {
-        yp1 = 0, yp2 = 0, xp1 = 0, xp2 = 0;
-        for (j = height - 1; j >= 0; j--)
-        {
-            tmp2[i][j] = (a3 * xp1 + a1 * xp2 + b1 * yp1 + b2 * yp2);
-            xp2 = xp1;
-            xp1 = in[i][j];
-            yp2 = yp1;
-            yp1 = tmp2[i][j];
-        }
+        tmp2[i][j] = (a3 * xp1 + a1 * xp2 + b1 * yp1 + b2 * yp2);
+        xp2 = xp1;
+        xp1 = in[i][j];
+        yp2 = yp1;
+        yp1 = tmp2[i][j];
     }
     return NULL;
 }
@@ -164,7 +218,32 @@ void *L4(void *data)
     int width = dataBis[0];
     int height = dataBis[1];
 
-    int i, j;
+    int j;
+
+    pthread_t *pLines = malloc(sizeof(pthread_t) * height);
+
+    for (j = 0; j < height; j++)
+    {
+        int tab[2];
+        tab[0] = width;
+        tab[1] = j;
+        pthread_create(&pLines[j], NULL, L4_line, tab);
+    }
+    for (j = 0; j < height; j++)
+    {
+        pthread_join(pLines[j], NULL);
+    }
+
+    return NULL;
+}
+
+void *L4_line(void *data)
+{
+    int *dataBis = data;
+
+    int width = dataBis[0];
+    int j = dataBis[1];
+    int i;
 
     float tm1, ym1, ym2;
     float a5, a6;
@@ -175,22 +254,17 @@ void *L4(void *data)
     b1 = ((0.840896));
     b2 = (float) ((-0.606531));
 
-    // L4:
-
-    for (j = 0; j < height; j++)
+    tm1 = 0, ym1 = 0, ym2 = 0;
+    for (i = 0; i < width; i++)
     {
-        tm1 = 0, ym1 = 0, ym2 = 0;
-        for (i = 0; i < width; i++)
-        {
-            tmp4[i][j] = (a5 * tmp3[i][j] + a6 * tm1 + b1 * ym1 + b2 * ym2);
-            tm1 = tmp3[i][j];
-            ym2 = ym1;
-            ym1 = tmp4[i][j];
-        }
+        tmp4[i][j] = (a5 * tmp3[i][j] + a6 * tm1 + b1 * ym1 + b2 * ym2);
+        tm1 = tmp3[i][j];
+        ym2 = ym1;
+        ym1 = tmp4[i][j];
     }
-
     return NULL;
 }
+
 
 void *L5(void *data)
 {
@@ -199,7 +273,33 @@ void *L5(void *data)
     int width = dataBis[0];
     int height = dataBis[1];
 
-    int i, j;
+    int j;
+
+    // L5:
+
+    pthread_t *pLines = malloc(sizeof(pthread_t) * height);
+
+    for (j = 0; j < height; j++)
+    {
+        int tab[2];
+        tab[0] = width;
+        tab[1] = j;
+        pthread_create(&pLines[j], NULL, L5_line, tab);
+    }
+    for (j = 0; j < height; j++)
+    {
+        pthread_join(pLines[j], NULL);
+    }
+    return NULL;
+}
+
+void *L5_line(void *data)
+{
+    int *dataBis = data;
+
+    int width = dataBis[0];
+    int j = dataBis[1];
+    int i;
 
     float tp1, tp2;
     float yp1, yp2;
@@ -211,22 +311,19 @@ void *L5(void *data)
     b1 = ((0.840896));
     b2 = (float) ((-0.606531));
 
-    // L5:
-
-    for (j = 0; j < height; j++)
+    tp1 = 0, tp2 = 0, yp1 = 0, yp2 = 0;
+    for (i = width - 1; i >= 0; i--)
     {
-        tp1 = 0, tp2 = 0, yp1 = 0, yp2 = 0;
-        for (i = width - 1; i >= 0; i--)
-        {
-            tmp5[i][j] = (a7 * tp1 + a8 * tp2 + b1 * yp1 + b2 * yp2);
-            tp2 = tp1;
-            tp1 = tmp3[i][j];
-            yp2 = yp1;
-            yp1 = tmp5[i][j];
-        }
+        tmp5[i][j] = (a7 * tp1 + a8 * tp2 + b1 * yp1 + b2 * yp2);
+        tp2 = tp1;
+        tp1 = tmp3[i][j];
+        yp2 = yp1;
+        yp1 = tmp5[i][j];
     }
     return NULL;
 }
+
+
 
 
 void oracle(int width, int height)
